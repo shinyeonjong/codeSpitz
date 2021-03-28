98f7cc81657626bfbfe16c968141c7fb80760023
shinyeonjong <yjshin81002@gmail.com>
2021-03-29 01:08:48 +0900

    modify array argument

--- codeSpitz/0323/tailRecursion.js.js
+++ codeSpitz/0323/tailRecursion.js.js
@@ -1,5 +1,5 @@
-const sum = arr => _sum(arr, arr.length - 1, 0)
-const _sum = (arr, index, accumulator) => index > 0
+const sum = arr => _sum(arr, arr.length - 1)
+const _sum = (arr, index, accumulator = 0) => index > 0
     ? _sum(arr, index - 1, accumulator + arr[index])
     : accumulator + arr[index]
 sum([1, 2, 3, 4, 5, 6])
